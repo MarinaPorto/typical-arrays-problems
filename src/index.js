5fe9ef29d4bc5284c2ebbd8678834b57cb33ca2f
MarinaPorto <marina.x.sea@gmail.com>
2020-10-02 23:16:44 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (typeof array != "undefined"
+    && array.length > 0) {
+    let minNumber = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (minNumber > array[i]) {
+        minNumber = array[i];
+      };
+    };
+    return minNumber;
+  }
+  else {
+    return 0
+  };
+};
 
-exports.max = function max (array) {
-  return 0;
-}
 
-exports.avg = function avg (array) {
-  return 0;
-}
+
+
+exports.max = function max(array) {
+  if (typeof array != "undefined"
+    && array.length > 0) {
+    let maxNumber = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (maxNumber < array[i]) {
+        maxNumber = array[i];
+      };
+    };
+    return maxNumber;
+  }
+  else {
+    return 0
+  };
+};
+
+
+
+
+exports.avg = function avg(array) {
+  if (typeof array != "undefined"
+    && array.length > 0) {
+    let middle = 0;
+    for (let i = 0; i < array.length; i++) {
+      middle += array[i];
+    };
+    return middle / array.length;
+  }
+  else {
+    return 0
+  };
+};
+
+
+
